e778b17ceca0fb8a99b2435dc56d94c4bbfc6cd3
Michael Davidsaver <mdavidsaver@bnl.gov>
2009-10-08 18:25:42 +0200

    566: seq wrong arguments
    committer: Michael Davidsaver <mdavidsaver@localhost.localdomain>

--- iocBoot/ioc566test/st.cmd
+++ iocBoot/ioc566test/st.cmd
@@ -9,9 +9,9 @@ stc566simple(1, 2, 0x100)
 # samples channges 0->3
 # 1 sample each
 # in order
-seq566set(1, 0, 1, 1, 0)
-seq566set(1, 1, 2, 1, 0)
-seq566set(1, 2, 3, 1, 0)
-seq566set(1, 3, 4, 1, 0)
+seq566set(1, 0, 1, 1, 1)
+seq566set(1, 1, 1, 2, 1)
+seq566set(1, 2, 1, 3, 1)
+seq566set(1, 3, 1, 4, 1)
 
 iocInit()
