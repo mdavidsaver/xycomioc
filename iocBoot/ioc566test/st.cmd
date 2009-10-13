5b6dd9f15a9bb95d0a643c8be05b3058830f963e
Michael Davidsaver <mdavidsaver@bnl.gov>
2009-10-13 16:51:36 +0200

    566: update test IOC for 2 cards
    committer: Michael Davidsaver <mdavidsaver@localhost.localdomain>

--- iocBoot/ioc566test/st.cmd
+++ iocBoot/ioc566test/st.cmd
@@ -5,38 +5,47 @@ xytest_registerRecordDeviceDriver(pdbbase)
 dbg566 = 2
 
 xycom566setup(1, 0xf800, 0xa00000, 3, 0xf2, 1)
-#xycom566setup(2, 0xf400, 0xb00000, 3, 0xe0, 1)
+xycom566setup(2, 0xf400, 0xb00000, 3, 0xe0, 1)
 
-stc566simple(1, 2, 0x100)
+############### ADC card 1 ####################
 
-# samples channges 0->3
+#stc566simple(1, 2, 0x100)
+stc566seqmulti(1, 2, 0x100, 0x4800)
+
+# samples channels 0->3
+# 40 sample each
+# in order w/ breaks between 0/1, 1/2, and 2/3
+#
+# sample channels 4,5
 # 1 sample each
-# in order
-seq566set(1, 0, 40, 1, 1)
-#seq566set(1, 1, 40, 2, 2)
-#seq566set(1, 2, 40, 3, 3)
-#seq566set(1, 3, 40, 4, 4)
-#seq566set(1, 4, 1, 5, 1)
-#seq566set(1, 5, 1, 6, 1)
+seq566set(1, 0, 40, 1, 1, 40)
+seq566set(1, 1, 40, 2, 2, 40)
+seq566set(1, 2, 40, 3, 3, 40)
+seq566set(1, 3, 40, 4, 4)
+seq566set(1, 4, 1, 5, 1)
+seq566set(1, 5, 1, 6, 1)
+
+############### ADC card 2 ####################
+
+#stc566simple(2, 2, 0x100)
+stc566seqmulti(2, 2, 0x100, 0x4800)
+
+# samples channels 0->3
+# 40 sample each
+# in order w/ breaks between 0/1, 1/2, and 2/3
+#
+# sample channels 4,5
+# 1 sample each
+seq566set(2, 0, 40, 1, 1, 40)
+seq566set(2, 1, 40, 2, 2, 40)
+seq566set(2, 2, 40, 3, 3, 40)
+seq566set(2, 3, 40, 4, 4)
+seq566set(2, 4, 1, 5, 1)
+seq566set(2, 5, 1, 6, 1)
 
 xycom566finish()
 
-dbLoadRecords("db/xy566base.db","P=,C=1")
-
-dbLoadRecords("db/xy566gain.db","P=,CH=ch1:,C=1,N=0")
-dbLoadRecords("db/xy566val.db","P=,CH=ch1,C=1,N=0")
-dbLoadRecords("db/xy566wave.db","P=,CH=ch1:wave,C=1,N=0,L=40")
-
-#dbLoadRecords("db/xy566gain.db","P=,CH=ch2:,C=1,N=1")
-#dbLoadRecords("db/xy566val.db","P=,CH=ch2,C=1,N=1")
-#dbLoadRecords("db/xy566wave.db","P=,CH=ch2:wave,C=1,N=1,L=40")
-
-#dbLoadRecords("db/xy566gain.db","P=,CH=ch3:,C=2,N=0")
-#dbLoadRecords("db/xy566val.db","P=,CH=ch3,C=1,N=2")
-#dbLoadRecords("db/xy566wave.db","P=,CH=ch3:wave,C=1,N=2,L=40")
-
-#dbLoadRecords("db/xy566gain.db","P=,CH=ch4:,C=1,N=3")
-#dbLoadRecords("db/xy566val.db","P=,CH=ch4,C=1,N=3")
-#dbLoadRecords("db/xy566wave.db","P=,CH=ch4:wave,C=1,N=3,L=40")
+dbLoadRecords("db/xy566lnl.db","P=spec:adc1:,C=1")
+dbLoadRecords("db/xy566lnl.db","P=spec:adc2:,C=2")
 
 iocInit()
